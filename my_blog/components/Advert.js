7929d7ff62bfc715f45865a58e940e211a9e30c0
xuwenqiang <xuwenqiang1@tal.com>
2020-12-03 13:33:23 +0800

    fix

---
+++ my_blog/components/Advert.js
@@ -0,0 +1,21 @@
+import "../styles/components/advert.scss"
+const Advert = () => {
+  return (
+    <div className="ad-div comm-box">
+      <div>
+        <img src="https://weiliicimg6.pstatp.com/weili/l/905571520588546187.webp" alt="" width="100%" />
+      </div>
+      <div>
+        <img src="https://weiliicimg6.pstatp.com/weili/l/905571520588546187.webp" alt="" width="100%" />
+      </div>
+      <div>
+        <img src="https://weiliicimg6.pstatp.com/weili/l/905571520588546187.webp" alt="" width="100%" />
+      </div>
+      <div>
+        <img src="https://weiliicimg6.pstatp.com/weili/l/905571520588546187.webp" alt="" width="100%" />
+      </div>
+
+    </div>
+  )
+}
+export default Advert
---
+++ my_blog/pages/_app.js
@@ -0,0 +1,8 @@
+import MyApp from 'next/app'
+import 'antd/dist/antd.css'
+import '../styles/pages/common.scss'
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
+
+export default MyApp
